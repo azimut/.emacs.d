dc090c5b9029d5663c6f51a64a5722ca219af300
azimut <azimut.github@protonmail.com>
2020-05-20 06:01:02 -0300

    Minor

--- lang/glsl.el
+++ lang/glsl.el
@@ -4,6 +4,18 @@
 
 (use-package glsl-mode
   :ensure t
+  :mode (("\\.pix\\'"   . glsl-mode)
+         ("\\.comp\\'"  . glsl-mode)
+         ("\\.usf\\'"   . glsl-mode)
+         ("\\.ush\\'"   . glsl-mode)
+         ("\\.vert\\'"  . glsl-mode)
+         ("\\.frag\\'"  . glsl-mode)
+         ("\\.geom\\'"  . glsl-mode)
+         ("\\.glsl\\'"  . glsl-mode)
+         ("\\.vs\\'"    . glsl-mode)
+         ("\\.fs\\'"    . glsl-mode)
+         ("\\.hlsl\\'"  . glsl-mode)
+         ("\\.hlsli\\'" . glsl-mode))
   :config
   (add-hook 'glsl-mode-hook
             (lambda ()
@@ -12,22 +24,7 @@
               (setq-local helm-dash-docsets '("OpenGL4"))
               (smartparens-strict-mode +1)
               (sp-use-paredit-bindings)
-              (aggressive-indent-mode +1)))
-  ;; g3d engine
-  (add-to-list 'auto-mode-alist '("\\.pix\\'"   . glsl-mode))
-  (add-to-list 'auto-mode-alist '("\\.comp\\'"  . glsl-mode))
-  ;; Unreal (though is actually hlsl...)
-  (add-to-list 'auto-mode-alist '("\\.usf\\'"   . glsl-mode))
-  (add-to-list 'auto-mode-alist '("\\.ush\\'"   . glsl-mode))
-  ;;
-  (add-to-list 'auto-mode-alist '("\\.vert\\'"  . glsl-mode))
-  (add-to-list 'auto-mode-alist '("\\.frag\\'"  . glsl-mode))
-  (add-to-list 'auto-mode-alist '("\\.geom\\'"  . glsl-mode))
-  (add-to-list 'auto-mode-alist '("\\.glsl\\'"  . glsl-mode))
-  (add-to-list 'auto-mode-alist '("\\.vs\\'"    . glsl-mode))
-  (add-to-list 'auto-mode-alist '("\\.fs\\'"    . glsl-mode))
-  (add-to-list 'auto-mode-alist '("\\.hlsl\\'"  . glsl-mode))
-  (add-to-list 'auto-mode-alist '("\\.hlsli\\'" . glsl-mode)))
+              (aggressive-indent-mode +1))))
 
 (use-package shader-mode
   :ensure t

8eb33109896c376eb9cd50615866febbc7454607
azimut <azimut.github@protonmail.com>
2021-02-05 01:51:27 -0300

    go lsp

--- lang/go.el
+++ lang/go.el
@@ -1,77 +1,69 @@
-(use-package ob-go)
-(use-package go-eldoc)
-(use-package gotest)
-(use-package flymake-go)
+;; OS:
+;; GO111MODULE=on go get -v golang.org/x/tools/gopls@latest
+;; go get -u -v github.com/godoctor/godoctor
+;; go get -u -v golang.org/x/tools/cmd/guru
+
+(use-package go-mode
+  :config
+  (define-key go-mode-map (kbd "M-p") #'flycheck-previous-error)
+  (define-key go-mode-map (kbd "M-n") #'flycheck-next-error)
+  (define-key go-mode-map (kbd "C-c C-d") #'lsp-describe-thing-at-point))
+
+(use-package ob-go)    ;; Org-mode Go support
+
+(use-package godoctor) ;; Refactor
 (use-package go-guru
   :init
   (add-hook 'go-mode-hook #'go-guru-hl-identifier))
-(use-package go-dlv
-  :commands (dlv-current-func dlv)
-  ;;:init
-  ;;(setq go-guru-command "/usr/lib/go/bin/guru")
-  )
-(use-package auto-complete)
-(use-package go-autocomplete)
-;;(use-package company-go :ensure t)
-(use-package gorepl-mode :after go-mode
-  :config
-  (add-hook
-   'gorepl-mode-hook
-   (lambda ()
-     (add-to-list 'ac-modes 'gorepl-mode)
-     (add-hook 'gorepl-mode-hook #'(lambda () (add-to-list 'ac-sources 'ac-source-go)))
-     (smartparens-strict-mode +1)
-     (sp-use-paredit-bindings))))
-;; (custom-set-faces
-;;  '(company-preview
-;;    ((t (:foreground "darkgray" :underline t))))
-;;  '(company-preview-common
-;;    ((t (:inherit company-preview))))
-;;  '(company-tooltip
-;;    ((t (:background "lightgray" :foreground "black"))))
-;;  '(company-tooltip-selection
-;;    ((t (:background "steelblue" :foreground "white"))))
-;;  '(company-tooltip-common
-;;    ((((type x)) (:inherit company-tooltip :weight bold))
-;;     (t (:inherit company-tooltip))))
-;;  '(company-tooltip-common-selection
-;;    ((((type x)) (:inherit company-tooltip-selection :weight bold))
-;;     (t (:inherit company-tooltip-selection)))))
-(use-package go-mode
-  :init
-  (add-hook 'go-mode-hook
-            (lambda ()
-              (setq-local tab-width 4)
-              (setq-local prettify-symbols-alist '(("func" . 955)
-                                                   ("<-"   . ?←)))
-              ;;
-              (yas-minor-mode +1)
-              (flycheck-mode +1)
-              (define-key go-mode-map (kbd "M-p") #'flycheck-previous-error)
-              (define-key go-mode-map (kbd "M-n") #'flycheck-next-error)
-              ;;
-              (require 'go-autocomplete)
-              (require 'auto-complete-config)
-              (ac-config-default)
-              ;;
-              ;; (set (make-local-variable 'company-backends) '(company-go))
-              ;; (company-mode)
-              ;;
-              (go-eldoc-setup)
-              (smartparens-strict-mode +1)
-              (sp-use-paredit-bindings)
-              (setq gofmt-command "goimports")
-              (setq godoc-reuse-buffer t)
-              (add-hook 'before-save-hook #'gofmt-before-save)))
-  :config
-  ;; (setq company-tooltip-limit 20)                      ; bigger popup window
-  ;; (setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
-  ;; (setq company-echo-delay 0)                          ; remove annoying blinking
-  ;; (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
-  ;; (define-key go-mode-map (kbd "C-x f") 'go-test-current-file)
-  ;; (define-key go-mode-map (kbd "C-x t") 'go-test-current-test)
-  ;; (define-key go-mode-map (kbd "C-x p") 'go-test-current-project)
-  ;; (define-key go-mode-map (kbd "C-x b") 'go-test-current-benchmark)
-  (define-key go-mode-map (kbd "M-.")     #'godef-jump)
-  (define-key go-mode-map (kbd "C-c C-k") #'go-run)
-  (define-key go-mode-map (kbd "C-c C-d") #'godoc-at-point))
+
+(use-package company-go)
+(use-package flycheck-golangci-lint)
+
+(defun gofly-setup ()
+  (setq flycheck-disabled-checkers '(go-gofmt
+                                     go-golint
+                                     go-vet
+                                     go-errcheck
+                                     go-staticcheck
+                                     go-unconvert))
+  (flycheck-golangci-lint-setup)
+  (flycheck-add-next-checker 'go-build '(warning . golangci-lint) t)
+  (flycheck-add-next-checker 'go-test  '(warning . golangci-lint) t))
+(add-hook 'go-mode-hook 'gofly-setup t)
+
+(defun go-config ()
+  (company-mode +1)
+  (smartparens-strict-mode +1)
+  (sp-use-paredit-bindings)
+  (setq-local prettify-symbols-alist '(("func" . 955)
+                                       ("<-"   . ?←)))
+  (setq-local tab-width 4)
+  (setq-local lsp-diagnostics-provider :none)
+  (set (make-local-variable 'company-backends) '(company-capf)))
+(add-hook 'go-mode-hook #'go-config)
+
+(add-hook 'go-mode-hook #'lsp-deferred)
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;; dap-go ???
+
+;; (use-package gotest)
+;; (use-package gorepl-mode :after go-mode
+;;   :config
+;;   (add-hook
+;;    'gorepl-mode-hook
+;;    (lambda ()
+;;      (add-to-list 'ac-modes 'gorepl-mode)
+;;      (add-hook 'gorepl-mode-hook #'(lambda () (add-to-list 'ac-sources 'ac-source-go))))))
+
+;; (use-package go-mode
+;;   :config
+;;   ;; (setq company-tooltip-limit 20)                      ; bigger popup window
+;;   ;; (setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+;;   ;; (setq company-echo-delay 0)                          ; remove annoying blinking
+;;   ;; (setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+;;   (define-key go-mode-map (kbd "C-c C-k") #'go-run)
+;;   )

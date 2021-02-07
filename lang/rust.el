8989b38c2303ee21e5390a2d66a13b10b51cee03
azimut <azimut.github@protonmail.com>
2021-02-07 15:52:38 -0300

    rust++

---
+++ lang/rust.el
@@ -0,0 +1,18 @@
+;; OS: install "rls"
+
+(use-package toml-mode)
+(use-package lsp-mode)
+
+(defun rust-config ()
+  (rust-enable-format-on-save)
+  (smartparens-strict-mode +1)
+  (sp-use-paredit-bindings)
+  ;; This fixes company behevior one use::
+  ;; Default: (32 41 46)
+  (setq-local company-auto-complete-chars nil))
+
+(use-package rust-mode
+  :hook (rust-mode . lsp)
+  :init
+  (add-hook #'rust-mode-hook
+            #'rust-config))

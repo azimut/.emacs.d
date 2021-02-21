a42e1fc440f3566e26c8faa3ab92232bf46c63d9
azimut <azimut.github@protonmail.com>
2021-02-21 13:35:20 -0300

    haskell lsp

--- lang/haskell.el
+++ lang/haskell.el
@@ -1,12 +1,17 @@
+;; OS: hlint
+
+(use-package lsp-haskell
+  :custom
+  (lsp-haskell-server-path "haskell-language-server")
+  (lsp-haskell-server-args '("-d" "-l" "/tmp/hls.log" "-j2")))
+
 (use-package haskell-mode
   :init
   (add-hook 'haskell-mode-hook #'electric-pair-mode))
 
-(use-package format-all
-  :init
-  (add-hook 'haskell-mode-hook #'format-all-mode))
-
-(use-package haskell-cabal-mode
-  :ensure nil
-  :bind ((:map haskell-cabal-mode-map ("C-c M-o" . haskell-interactive-mode-clear))))
+(add-hook 'haskell-mode-hook #'lsp)
+(add-hook 'haskell-literate-mode-hook #'lsp)
+;; (use-package haskell-cabal-mode
+;;   :ensure nil
+;;   :bind ((:map haskell-cabal-mode-map ("C-c M-o" . haskell-interactive-mode-clear))))
 

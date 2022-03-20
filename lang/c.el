3f493051cca5792ee7286565aa5dabe1f3d5b7e6
azimut <azimut.github@protonmail.com>
2022-03-20 19:53:34 -0300

    c: ccls, lsp config

--- lang/c.el
+++ lang/c.el
@@ -1,17 +1,28 @@
 ;; Os: install "ccls"
+;; Enable it per buffer with M-lsp
 
 (use-package ccls
   :init
-  (setq ccls-executable "/usr/bin/ccls")
+  ;;(setq ccls-executable "/usr/bin/ccls")
+  (setq ccls-executable "/usr/local/bin/ccls")
   (setq ccls-initialization-options
-        '(:index (:comments 2) :completion (:detailedLabel t))))
+        '(:index
+          (:comments 2 :trackDependecy 1 :threads 2)
+          :completion
+          (:detailedLabel t))))
 
 (defun c-config ()
   (smartparens-strict-mode +1)
   (sp-use-paredit-bindings)
   (require 'ccls)
   (lsp-mode +1)
-  (add-hook 'before-save-hook #'lsp-format-buffer t t))
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (setq-local company-auto-commit-chars nil)
+  (display-fill-column-indicator-mode +1))
+
+(define-key c-mode-map (kbd "C-c C-d") #'lsp-describe-thing-at-point)
+(define-key c-mode-map (kbd "C-c C-h") #'helm-dash-at-point)
+
 
 (add-hook #'c-mode-hook #'c-config)
 ;; (add-hook #'c++-mode-hook   #'my-cmode-hook)

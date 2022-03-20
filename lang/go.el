524257f65f766af39840a717e63472db9ed9e5d7
azimut <azimut.github@protonmail.com>
2022-03-20 19:51:54 -0300

    golines

--- lang/go.el
+++ lang/go.el
@@ -8,20 +8,21 @@
 ;; go get -u golang.org/x/tools/cmd/guru
 ;; go get -u github.com/cweill/gotests/...
 ;; go get -u github.com/fatih/gomodifytags
+;; go get -u github.com/segmentio/golines
 (use-package go-mode
   :config
-  (define-key go-mode-map (kbd "M-p") #'flycheck-previous-error)
-  (define-key go-mode-map (kbd "M-n") #'flycheck-next-error)
+  ;; (define-key go-mode-map (kbd "M-p") #'flycheck-previous-error)
+  ;; (define-key go-mode-map (kbd "M-n") #'flycheck-next-error)
   (define-key go-mode-map (kbd "C-c C-d") #'lsp-describe-thing-at-point))
 
 (use-package ob-go)    ;; Org-mode Go support
 
 (use-package godoctor) ;; Refactor
-(use-package go-guru
-  :init
-  (add-hook 'go-mode-hook #'go-guru-hl-identifier))
-
-(use-package flycheck-golangci-lint)
+;; (use-package go-guru
+;;   :init
+;;   (add-hook 'go-mode-hook #'go-guru-hl-identifier))
+(add-hook 'go-mode-hook 'gofly-setup t)
+;;(use-package flycheck-golangci-lint)
 (defun gofly-setup ()
   (setq flycheck-disabled-checkers '(go-gofmt
                                      go-golint
@@ -32,23 +33,29 @@
   (flycheck-golangci-lint-setup)
   (flycheck-add-next-checker 'go-build '(warning . golangci-lint) t)
   (flycheck-add-next-checker 'go-test  '(warning . golangci-lint) t))
-(add-hook 'go-mode-hook 'gofly-setup t)
 
 (defun go-config ()
   (smartparens-strict-mode +1)
   (sp-use-paredit-bindings)
-  (setq-local prettify-symbols-alist '(("func" . 955)
-                                       ("<-"   . ?←)))
+  (setq-local prettify-symbols-alist '(("func" . 955) ("<-"   . ?←)))
   (setq-local tab-width 4)
-  (setq-local company-auto-complete-chars nil);; has to be something better
   (setq-local lsp-diagnostics-provider :none)
+  (setq-local lsp-register-custom-settings '(("gopls.completeUnimported" t t) ("gopls.staticcheck" t t)))
+  (setq-local company-auto-complete-chars nil);; has to be something better
+  ;; (setq company-insertion-triggers nil)
+  (setq company-auto-complete nil)
   (setq company-go-show-annotation t)
   (set (make-local-variable 'company-backends) '(company-capf))
-  (company-mode +1))
+  (flycheck-mode +1)
+  ;;(company-mode +1)
+  )
 (add-hook 'go-mode-hook #'go-config)
 
 (add-hook 'go-mode-hook #'lsp-deferred)
+
 (defun lsp-go-install-save-hooks ()
+  (setq gofmt-command "golines")
+  (add-hook 'before-save-hook #'gofmt-before-save)
   (add-hook 'before-save-hook #'lsp-format-buffer t t)
   (add-hook 'before-save-hook #'lsp-organize-imports t t))
 (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)

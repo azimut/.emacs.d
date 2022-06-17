939e5a8013d737b59a39139f74d1318868dce96e
azimut <azimut.github@protonmail.com>
2022-06-17 04:50:57 -0300

    html: LSP - disable eslint, COMPANY: disable auto insert

--- lang/html.el
+++ lang/html.el
@@ -37,7 +37,8 @@ snippet, or `emmet-expand-yas'/`emmet-expand-line', depending on whether
   (smartparens-mode +1)
   ;; (company-mode +1)
   ;; (set (make-local-variable 'company-backends) '(company-css company-web-html))
-  (setq-local company-insertion-on-trigger t)
+  (setq-local lsp-disabled-clients '(eslint))
+  (setq-local company-insertion-on-trigger nil)
   (setq-local lsp-eldoc-enable-hover nil) ;; Too busy
   (setq-local company-insertion-triggers '(?\  ?\)))
   (emmet-mode +1))

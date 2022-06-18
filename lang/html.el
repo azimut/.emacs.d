7ec12b1aea73c988c3baac75e50ead9cc59c1e24
azimut <azimut.github@protonmail.com>
2022-06-17 22:25:05 -0300

    html: minor reformat

--- lang/html.el
+++ lang/html.el
@@ -1,9 +1,4 @@
 ;; lsp-install-server >> html-ls
-;; https://github.com/doomemacs/doomemacs/blob/develop/modules/lang/web/
-;; https://github.com/syl20bnr/spacemacs/tree/develop/layers/%2Blang/html
-;;
-;; web-beautify  : npm install -g js-beautify
-;; format-all    : tidy/stylelint(css) http://www.html-tidy.org/
 
 (use-package impatient-mode)
 
@@ -21,7 +16,6 @@ snippet, or `emmet-expand-yas'/`emmet-expand-line', depending on whether
           #'indent-for-tab-command)
          (#'emmet-expand-yas))))
 
-;; https://github.com/smihica/emmet-mode#html-abbreviations
 (use-package emmet-mode
   :bind (:map emmet-mode-keymap
               ("<tab>" . +web/indent-or-yas-or-emmet-expand)
@@ -33,39 +27,40 @@ snippet, or `emmet-expand-yas'/`emmet-expand-line', depending on whether
        (eq web-mode-auto-close-style 3)))
 
 (defun web-config ()
-  (smartparens-mode +1)
   (setq-local lsp-disabled-clients '(eslint))
-  (setq-local company-insertion-on-trigger nil)
   (setq-local lsp-eldoc-enable-hover nil) ;; Too busy
+  (setq-local company-insertion-on-trigger nil)
   (setq-local company-insertion-triggers '(?\  ?\)))
+  (smartparens-mode +1)
   (emmet-mode +1))
 
-;; https://github.com/Fuco1/smartparens/issues/80
 (defun radian-enter-and-indent-sexp (&rest _ignored)
-  "Insert an extra newline after point, and reindent."
+  "Insert an extra newline after point, and reindent.
+   https://github.com/Fuco1/smartparens/issues/80"
   (newline)
   (indent-according-to-mode)
   (forward-line -1)
   (indent-according-to-mode))
 
-;; https://web-mode.org/
 (use-package web-mode
   :mode "\\.[px]?html?\\'"
   :bind (:map
          web-mode-map
          ("C-M-t" . web-mode-element-transpose)
          ("C-c C-d" . lsp-describe-thing-at-point))
+  :custom
+  (lsp-html-format-enable                      nil "BUG?: hangs up <style> editing for 2 seconds")
+  (web-mode-block-padding                      2)
+  (web-mode-code-indent-offset                 2)
+  (web-mode-css-indent-offset                  2)
+  (web-mode-markup-indent-offset               2)
+  (web-mode-comment-style                      2)
+  (web-mode-auto-close-style                   1)
+  (web-mode-enable-css-colorization            nil "LSP already has colors")
+  (web-mode-enable-current-element-highlight   nil "LSP already has it")
+  (web-mode-enable-html-entities-fontification t)
+  :init
+  (add-hook #'web-mode-hook #'web-config)
   :config
   (sp-local-pair 'web-mode "<" ">" :unless '(:add +web-is-auto-close-style-3))
-  (sp-local-pair 'web-mode "{" nil :post-handlers '((radian-enter-and-indent-sexp "C-j")))
-  (setq lsp-html-format-enable nil) ; BUG?: hangs up <style> editing for 2 seconds
-  (setq web-mode-enable-css-colorization nil) ;; LSP already has colors
-  (setq web-mode-enable-current-element-highlight nil) ;; LSP already has it
-  (setq web-mode-enable-html-entities-fontification t
-        web-mode-auto-close-style 1)
-  (setq web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset    2
-        web-mode-code-indent-offset   2
-        web-mode-block-padding        2
-        web-mode-comment-style        2)
-  (add-hook #'web-mode-hook #'web-config))
+  (sp-local-pair 'web-mode "{" nil :post-handlers '((radian-enter-and-indent-sexp "C-j"))))

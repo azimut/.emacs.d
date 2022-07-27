aee52922c9899c779e13fb0a401af5b87650315e
azimut <azimut.github@protonmail.com>
2022-07-27 02:22:23 -0300

    lsp python

--- lang/python.el
+++ lang/python.el
@@ -1,3 +1,18 @@
+;; pip install --user python-lsp-server
+
+(use-package python
+  :ensure nil
+  :hook (python-mode . lsp)
+  :hook (python-mode . smartparens-mode)
+  :config
+  (define-key python-mode-map (kbd "DEL") nil) ; interferes with smartparens
+  (sp-local-pair 'python-mode "'" nil
+                 :unless '(sp-point-before-word-p
+                           sp-point-after-word-p
+                           sp-point-before-same-p)))
+
+(use-package lsp-pyright)
+
 ;; jupyter elpy
 (setenv "IPY_TEST_SIMPLE_PROMPT" "1")
 (setq python-shell-interpreter "jupyter"

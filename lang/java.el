87d389d3006b906a535cd9df0b452b0b52d81808
azimut <azimut.github@protonmail.com>
2022-07-27 02:22:37 -0300

    ruby/java draft

---
+++ lang/java.el
@@ -0,0 +1,4 @@
+;; https://github.com/syl20bnr/spacemacs/tree/develop/layers/%2Blang/java
+;; https://github.com/doomemacs/doomemacs/blob/master/modules/lang/java/README.org
+;; https://github.com/emacs-lsp/lsp-java
+(use-package lsp-java)

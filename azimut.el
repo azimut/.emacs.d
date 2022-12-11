9a7c12b4c6e94a8397cf48508a3c95d5ce4bb51c
azimut <azimut.github@protonmail.com>
2022-12-11 16:12:30 -0300

    add recentf for C-S-T

--- azimut.el
+++ azimut.el
@@ -163,6 +163,21 @@
          ("C-c C-+" . evil-numbers/inc-at-pt-incremental)
          ("C-c C--" . evil-numbers/dec-at-pt-incremental)))
 
+(use-package recentf
+  :bind ("C-S-t" . undo-kill-buffer)
+  :custom
+  (recentf-auto-cleanup 'never)
+  (recentf-save-file "~/.emacs.d/recentf")
+  (recentf-max-menu-items 10000)
+  (recentf-max-saved-items nil)
+  :config
+  (defun undo-kill-buffer (arg)
+    "Re-open the last buffer killed.  With ARG, re-open the nth buffer."
+    (interactive "p")
+    (find-file (nth (1- arg) recentf-list)))
+
+  (recentf-mode t))
+
 (add-hook 'makefile-mode (lambda () (setq-local whitespace-style '(face tabs empty))))
 
 (defun evince-open-pdf ()

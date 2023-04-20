47e04035cfbadac5793b4961fbe7c74fc122d64c
azimut <azimut.github@protonmail.com>
2023-04-20 06:47:24 -0300

    java: C-c to recompile, rainbow delimiters

--- lang/java.el
+++ lang/java.el
@@ -1,4 +1,6 @@
 ;; https://github.com/syl20bnr/spacemacs/tree/develop/layers/%2Blang/java
 ;; https://github.com/doomemacs/doomemacs/blob/master/modules/lang/java/README.org
 ;; https://github.com/emacs-lsp/lsp-java
-(use-package lsp-java)
+(use-package lsp-java
+  :bind (:map java-mode-map ("C-c C-c" . recompile))
+  :hook (java-mode . rainbow-delimiters-mode))

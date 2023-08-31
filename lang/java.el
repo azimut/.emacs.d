d7e95deb80df75ccab9ba164c813a3512e569198
azimut <azimut.github@protonmail.com>
2023-08-31 14:32:57 -0300

    java compile-command

--- lang/java.el
+++ lang/java.el
@@ -1,6 +1,24 @@
 ;; https://github.com/syl20bnr/spacemacs/tree/develop/layers/%2Blang/java
 ;; https://github.com/doomemacs/doomemacs/blob/master/modules/lang/java/README.org
 ;; https://github.com/emacs-lsp/lsp-java
+
+(defun java-config ()
+  (setq-local compile-command
+              (cond ((file-exists-p "pom.xml") ;; FIXME: parent path
+                     "mvn compile")
+                    (t (concat
+                        "javac "
+                        buffer-file-name
+                        " && java "
+                        (file-name-sans-extension
+                         (file-name-nondirectory
+                          buffer-file-name)))))))
+
+(use-package java-mode
+  :ensure nil
+  :hook (java-mode . smartparens-strict-mode)
+  :hook (java-mode . rainbow-delimiters-mode)
+  :hook (java-mode . java-config))
+
 (use-package lsp-java
-  :bind (:map java-mode-map ("C-c C-c" . recompile))
-  :hook (java-mode . rainbow-delimiters-mode))
+  :bind (:map java-mode-map ("C-c C-c" . recompile)))

9f9111008565fb1c0695603d6542de8196a65419
azimut <azimut.github@protonmail.com>
2023-09-05 08:41:28 -0300

    lisp: update indent variables to use sly- based ones

--- lang/lisp.el
+++ lang/lisp.el
@@ -45,52 +45,52 @@
 ;;   'redshank-align-forms-as-columns)
 
 (use-package sly
-  ;;:init
-  ;; (setq slime-lisp-implementations '((sbcl ("/usr/local/bin/sbcl")))
-  ;;       inferior-lisp-program "/usr/local/bin/sbcl"
-  ;;       slime-contribs '(slime-fancy))
-  ;; "modern" style
-  :hook (sly-mode . paredit-mode)
-  :hook (sly-mode . aggressive-indent-mode)
-  :hook (sly-mode . rainbow-delimiters-mode)
-  :bind (:map paredit-mode-map
-              ("RET" . nil)
-              ("C-j" . paredit-newline))
-  :config
-  (setq sly-lisp-implementations     '((sbcl  ("/usr/local/bin/sbcl"
-                                               ;;"--dynamic-space-size"
-                                               ;;"1024"
-                                               )))
-        ;;sly-complete-symbol-function 'sly-flex-completions;;'sly-simple-completions
-        inferior-lisp-program        "/usr/local/bin/sbcl"
-        ;;sly-contribs                 '(sly-fancy sly-macrostep)
-        sly-inhibit-pipelining       nil
-        sly-load-failed-fasl         'always
-        sly-description-autofocus    t)
-  (sly-setup)
-  ;; (add-hook 'sly--completion-display-mode-hook (lambda () (setq-local show-trailing-whitespace nil)))
-  (add-hook
-   'sly-mode-hook
-   (lambda ()
-     (setq-local lisp-indent-function 'common-lisp-indent-function)
-     (setq lisp-lambda-list-keyword-alignment             t
-           lisp-lambda-list-keyword-parameter-alignment   t
-           lisp-lambda-list-keyword-parameter-indentation 0
-           lisp-loop-indent-forms-like-keywords           t
-           lisp-loop-indent-subclauses                    nil)
-     ;; (setq redshank-align-slot-forms-list
-     ;;       '("defclass" "define-condition"
-     ;;         "define-subject"
-     ;;         "define-shader-subject" "define-shader-entity" "define-shader-pass"))
-     (display-fill-column-indicator-mode +1)
-     ))
-  (add-hook
-   'sly-mrepl-hook
-   (lambda ()
-     (setq-local mode-line-format nil)
-     ;; CEPL crashes if receives it, and i tend to do it as a tick...
-     (define-key sly-mrepl-mode-map (kbd "C-c C-c") nil)
-     (define-key sly-mrepl-mode-map (kbd "C-<escape>") #'sly-interrupt)
-     ;; (setq-local show-trailing-whitespace nil)
-     ;;(define-key slime-repl-mode-map (kbd "C-c C-d C-d") #'slime-describe-symbol)
-     ))  )
+    ;;:init
+    ;; (setq slime-lisp-implementations '((sbcl ("/usr/local/bin/sbcl")))
+    ;;       inferior-lisp-program "/usr/local/bin/sbcl"
+    ;;       slime-contribs '(slime-fancy))
+    ;; "modern" style
+    :hook (sly-mode . paredit-mode)
+    :hook (sly-mode . aggressive-indent-mode)
+    :hook (sly-mode . rainbow-delimiters-mode)
+    :bind (:map paredit-mode-map
+                ("C-M-p" . move-text-up)
+                ("RET" . nil)
+                ("C-j" . paredit-newline))
+    :config
+    (setq sly-lisp-implementations     '((sbcl  ("/usr/local/bin/sbcl"
+                                                 ;;"--dynamic-space-size"
+                                                 ;;"1024"
+                                                 )))
+          ;;sly-complete-symbol-function 'sly-flex-completions;;'sly-simple-completions
+          inferior-lisp-program        "/usr/local/bin/sbcl"
+          ;;sly-contribs                 '(sly-fancy sly-macrostep)
+          sly-inhibit-pipelining       nil
+          sly-load-failed-fasl         'always
+          sly-description-autofocus    t)
+    (sly-setup)
+    ;; (add-hook 'sly--completion-display-mode-hook (lambda () (setq-local show-trailing-whitespace nil)))
+    (add-hook
+     'sly-mode-hook
+     (lambda ()
+       (setq sly-lisp-lambda-list-keyword-alignment             t
+             sly-lisp-lambda-list-keyword-parameter-alignment   t
+             sly-lisp-lambda-list-keyword-parameter-indentation 2
+             sly-lisp-loop-indent-forms-like-keywords           t
+             sly-lisp-loop-indent-subclauses                    nil)
+       ;; (setq redshank-align-slot-forms-list
+       ;;       '("defclass" "define-condition"
+       ;;         "define-subject"
+       ;;         "define-shader-subject" "define-shader-entity" "define-shader-pass"))
+       (display-fill-column-indicator-mode +1)
+       ))
+    (add-hook
+     'sly-mrepl-hook
+     (lambda ()
+       (setq-local mode-line-format nil)
+       ;; CEPL crashes if receives it, and i tend to do it as a tick...
+       (define-key sly-mrepl-mode-map (kbd "C-c C-c") nil)
+       (define-key sly-mrepl-mode-map (kbd "C-<escape>") #'sly-interrupt)
+       ;; (setq-local show-trailing-whitespace nil)
+       ;;(define-key slime-repl-mode-map (kbd "C-c C-d C-d") #'slime-describe-symbol)
+       ))  )

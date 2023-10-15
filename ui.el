47cd698423c2026b932744c68a8371d43636b158
azimut <azimut.github@protonmail.com>
2023-10-15 04:53:46 -0300

    ui update

--- ui.el
+++ ui.el
@@ -23,11 +23,15 @@
 (add-hook 'vterm-mode-hook #'disable-trailing-highlight)
 (add-hook 'help-mode-hook #'disable-trailing-highlight)
 (add-hook 'eww-mode-hook #'disable-trailing-highlight)
-(add-hook 'sly-autodoc-mode-hook #'disable-trailing-highlight)
+;;(add-hook 'sly-autodoc-mode-hook #'disable-trailing-highlight)
 
 (setq read-file-name-completion-ignore-case t
       read-buffer-completion-ignore-case t
       completion-ignore-case t)
+
+;; (set-face-attribute 'mode-line nil :height 100)
+;; (set-face-attribute 'mode-line-inactive nil :height 100)
+
 
 ;; https://www.emacswiki.org/emacs/MenuBar
 (menu-bar-mode -1)
@@ -38,12 +42,14 @@
 
 (require 'whitespace)
 (global-whitespace-mode 1)
-(setq whitespace-style '(face empty))
 
+(setq whitespace-style '(face empty))
 (use-package neotree
   :bind ("C-0" . neotree-toggle)
+  ;;  neo-theme
   :custom
-  (neo-theme              'arrow)
+  (neo-theme              'classic)
+  (neo-show-hidden-files  t)
   (neo-hidden-regexp-list '("^\\." "\\.pyc$" "\\.fasl$" "~$" "^#.*#$" "\\.elc$" "\\.beam$" "\\.meta$")))
 
 (use-package imenu-list
@@ -58,6 +64,10 @@
   :init (vertico-mode +1))
 
 (use-package doom-modeline
+  :custom
+  (doom-modeline-height      1)
+  (doom-modeline-bar-width 0)
+  (doom-modeline-icon nil)
   :hook (after-init . doom-modeline-mode))
 
 ;; (use-package yascroll
@@ -71,7 +81,8 @@
   (nyan-bar-length            16)
   (nyan-animate-nyancat      nil))
 
-(lexical-let ((color-flag t))
+(set-face-foreground 'font-lock-comment-face "orange")
+(lexical-let ((color-flag))
   (defun toggle-color ()
     (interactive)
     (setf color-flag (not color-flag))
@@ -79,7 +90,7 @@
         (set-face-foreground 'font-lock-comment-face "orange")
       (set-face-foreground 'font-lock-comment-face "#696969"))))
 
-;; (set-cursor-color "#FF00FF")
+;;(set-cursor-color "#FF00FF")
 (set-cursor-color "#00FF00")
 
 (setq-default left-fringe-width 0 right-fringe-width 8
@@ -94,5 +105,9 @@
 (add-hook 'text-mode-hook (lambda () (setq-local mode-require-final-newline nil)))
 (add-hook 'package-menu-mode-hook (lambda () (hl-line-mode +1)))
 
-(setq display-line-numbers-type 'relative)
+(setq display-line-numbers-type 't)
 (setq display-line-numbers-grow-only t)
+
+(add-hook 'ibuffer-mode-hook (lambda () (hl-line-mode +1)))
+
+(column-number-mode +1)

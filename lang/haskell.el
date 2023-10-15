49d4d31fa2103433a1239a38f3e0af405cd27669
azimut <azimut.github@protonmail.com>
2023-10-15 04:32:52 -0300

    haskell: move interactive to his own use-package

--- lang/haskell.el
+++ lang/haskell.el
@@ -5,14 +5,21 @@
   (lsp-haskell-server-path "haskell-language-server")
   (lsp-haskell-server-args '("-d" "-l" "/tmp/hls.log" "-j2"))) ;; -j1 hangs
 
+(use-package haskell-interactive-mode
+  :ensure nil
+  :bind (:map
+         haskell-interactive-mode-map
+         ;; ("C-c M-o" . haskell-interactive-mode-clear)
+         ("C-c C-c" . recompile))
+  :config
+  (unbind-key "C-c C-c" haskell-interactive-mode-map))
+
 (use-package haskell-mode
   :bind (:map
          haskell-mode-map
+         ("C-j" . haskell-indentation-newline-and-indent)
          ("C-c C-d" . lsp-describe-thing-at-point)
-         ("C-c C-k" . haskell-process-load-file)
-         ;; haskell-interactive-mode-map
-         ;; ("C-c M-o" . haskell-interactive-mode-clear)
-         )
+         ("C-c C-k" . haskell-process-load-file))
   :init
   (add-hook 'haskell-mode-hook #'electric-pair-local-mode)
   :config
@@ -28,4 +35,3 @@
 ;; (use-package haskell-cabal-mode
 ;;   :ensure nil
 ;;   :bind ((:map haskell-cabal-mode-map ("C-c M-o" . haskell-interactive-mode-clear))))
-

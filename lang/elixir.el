f684a6f1fe4f170ff83e1ba0d4a6829500fd51d7
azimut <azimut.github@protonmail.com>
2023-11-20 23:23:31 -0300

    elixir: pretty symbols

--- lang/elixir.el
+++ lang/elixir.el
@@ -1,6 +1,20 @@
-;; Elixir
+(defun elixir-config ()
+  (setq-local prettify-symbols-alist
+              '(("->"  .  (?\s (Br . Bl) ?\s
+                               (Bl . Bl) ?- (Bc . Br) ?- (Bc . Bc) ?-
+                               (Bc . Bl) ?- (Br . Br) ?>))
+                ("<-"  .  (?\s (Br . Bl) ?\s
+                               (Bl . Bl) ?< (Bc . Br) ?- (Bc . Bc) ?-
+                               (Bc . Bl) ?- (Br . Br) ?-))
+                ("++"  .  (?\s (Br . Bl) ?\s
+                               (Bl . Bl) ?+ (Bc . Br) ?+ (Bc . Bc) ?-
+                               (Bc . Bl) ?+ (Br . Br) ?+))
+                ("=="  .  (?\s (Br . Bl) ?\s
+                               (Bl . Bl) ?= (Bc . Br) ?= (Bc . Bc) ?=
+                               (Bc . Bl) ?= (Br . Br) ?=)))))
 
 (use-package elixir-mode
+  :hook (elixir-mode . elixir-config)
   :config
   (define-key elixir-mode-map (kbd "SPC") #'yas-maybe-expand))
 
@@ -10,4 +24,3 @@
 ;;                     "\\(?:^\\|\\s-+\\)\\(?:do\\)")
 ;;                (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
 ;;                (ruby-end-mode +1)))
-

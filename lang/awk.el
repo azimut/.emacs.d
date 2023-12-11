f2913d85c0093692964c376b01a893a25eb57a8e
azimut <azimut.github@protonmail.com>
2023-12-11 08:19:41 -0300

    awk: pretty symbols and default compile command

--- lang/awk.el
+++ lang/awk.el
@@ -2,6 +2,19 @@
 (require 'awk-yasnippets)
 (eval-after-load 'yasnippet '(awk-yasnippets-initialize))
 
+(defun awk-config ()
+  (setq-local prettify-symbols-alist
+              '(("=="  .  (?\s (Br . Bl) ?\s
+                               (Bl . Bl) ?= (Bc . Br) ?= (Bc . Bc) ?=
+                               (Bc . Bl) ?= (Br . Br) ?=))
+                ("/="  .  (?\s (Br . Bl) ?\s
+                               (Bl . Bl) ?= (Bc . Br) ?= (Bc . Bc) ?/
+                               (Bc . Bl) ?= (Br . Br) ?=))
+                ("++"  .  (?\s (Br . Bl) ?\s
+                               (Bl . Bl) ?+ (Bc . Br) ?+ (Bc . Bc) ?-
+                               (Bc . Bl) ?+ (Br . Br) ?+))))
+  (setq-local compile-command (concat "time gawk -f " buffer-file-name)))
+
 ;; https://github.com/Beaglefoot/awk-language-server
 ;; $ npm install -g awk-language-server
 (use-package awk-mode
@@ -11,4 +24,5 @@
          ("C-c C-c" . recompile))
   :hook (awk-mode . lsp-mode)
   :hook (awk-mode . aggressive-indent-mode)
-  :hook (awk-mode . smartparens-strict-mode))
+  :hook (awk-mode . smartparens-strict-mode)
+  :hook (awk-mode . awk-config))

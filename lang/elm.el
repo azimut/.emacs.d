e6792db710a1fe1217e397b7124e61dc87bb11c5
azimut <azimut.github@protonmail.com>
2023-12-12 06:01:21 -0300

    elm: remove lsp noise

--- lang/elm.el
+++ lang/elm.el
@@ -1,4 +1,7 @@
 (defun elm-config ()
+  ;; https://emacs-lsp.github.io/lsp-mode/tutorials/how-to-turn-off/
+  (setq-local lsp-lens-enable nil) ; hide "exposed|references"
+  (setq-local lsp-completion-show-kind nil) ; hide "(Function)"
   (setq-local prettify-symbols-alist
               '(("->"  .  (?\s (Br . Bl) ?\s
                                (Bl . Bl) ?- (Bc . Br) ?- (Bc . Bc) ?-

86048bc20d8699c2aeabac5e0171758abd6fc63e
azimut <azimut.github@protonmail.com>
2024-03-16 15:30:47 -0300

    add tree-sitter-langs

--- prog.el
+++ prog.el
@@ -187,6 +187,7 @@ If the error list is visible, hide it.  Otherwise, show it."
   :config
   (global-tree-sitter-mode +1)
   (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+(use-package tree-sitter-langs)
 
 (use-package chatgpt-shell
   :config

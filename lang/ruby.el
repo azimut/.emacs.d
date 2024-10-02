0fc5419b220fad079be2ec215984561f4a1532e7
azimut <azimut.github@protonmail.com>
2024-10-01 21:13:54 -0300

    init ruby

--- lang/ruby.el
+++ lang/ruby.el
@@ -1,1 +1,11 @@
-;; sudo gem install solargraph
+;; sudo apt install pry
+;; sudo gem install solargraph rubocop
+
+(use-package ruby-mode
+  :ensure nil
+  :bind (:map
+         ruby-mode-map
+         ("C-c C-c" . recompile)))
+
+(use-package inf-ruby)
+(use-package robe)

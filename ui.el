7dce614ed245b5285ac61eb2057f7a4878aacb98
azimut <azimut.github@protonmail.com>
2024-11-09 16:45:27 -0300

    ui: cursor color, restore left fringe for git-gutter

--- ui.el
+++ ui.el
@@ -112,10 +112,10 @@
 ;;         (set-face-foreground 'font-lock-comment-face "orange")
 ;;       (set-face-foreground 'font-lock-comment-face "#696969"))))
 
-(set-cursor-color "#FF00FF")
-;;(set-cursor-color "#00FF00")
+;;(set-cursor-color "#FF00FF")
+(set-cursor-color "#00FF00")
 
-(setq-default left-fringe-width 0 right-fringe-width 8
+(setq-default left-fringe-width 8 right-fringe-width 8
               left-margin-width 1 right-margin-width 0)
 
 (set-fringe-style (quote (20 . 10)))

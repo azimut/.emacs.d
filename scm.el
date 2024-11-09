0529ac56e497550f5d12115adee2237a827de3a2
azimut <azimut.github@protonmail.com>
2024-11-09 16:48:02 -0300

    scm: default to left-fringe

--- scm.el
+++ scm.el
@@ -23,17 +23,13 @@
   (setq projectile-completion-system 'ivy
         projectile-sort-order 'recently-active))
 
-;; NOTE: -fringe needed (? crt.o on dnf gcc-x86_64-linux-gnu.x86_64
 (use-package git-gutter
   :hook (prog-mode . git-gutter-mode))
 
+;; NOTE: -fringe needed (? crt.o on dnf gcc-x86_64-linux-gnu.x86_64
 (use-package git-gutter-fringe
-  :custom (git-gutter-fr:side 'right-fringe)
   :config
   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
 
-;;(require 'git-gutter-fringe)
-;; (setq right-fringe-width 20)
-;; (setq left-fringe-width 20)

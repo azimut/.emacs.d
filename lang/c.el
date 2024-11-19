9ab980e44e20469c0a0961a8187a8ef2a12e00ee
azimut <azimut.github@protonmail.com>
2024-11-19 18:51:33 -0300

    c: move to eglot/corfu

--- lang/c.el
+++ lang/c.el
@@ -1,33 +1,16 @@
 ;; Os: install "ccls"
-;; Enable it per buffer with M-lsp
 
-;; (require 'dap-lldb)
-(require 'dap-codelldb)
-(require 'dap-gdb-lldb)
-
-(use-package ccls
-  :init
-  (setq ccls-executable "/snap/bin/ccls")
-  (setq ccls-initialization-options
-        '(:index
-          (:comments 2 :trackDependecy 1 :threads 2)
-          :completion
-          (:detailedLabel t)))
-  ;; :config
-  ;; (add-to-list 'ccls-root-files ".ccls")
-  ;; (add-to-list 'ccls-root-files "compile_commands.json")
-  )
+(setq gdb-many-windows t
+      gdb-debuginfod-enable-setting nil)
 
 (defun c-config ()
-  (require 'ccls)
   (smartparens-strict-mode +1)
-  (company-yasnippet +1)
-  (display-fill-column-indicator-mode +1)
-  (lsp-mode +1)
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  ;;(company-yasnippet +1)
+  (add-hook 'before-save-hook #'eglot-format-buffer t t)
+  (eglot-ensure)
+  (corfu-mode +1)
   (ligature-set-ligatures 'c-mode '("<=" ">=" "==" "!=" "&&" "||" "->"))
-  (setq-local lsp-enable-snippet nil) ; the quality is really poor for C
-  (setq-local company-auto-commit-chars nil)
+  ;; (setq-local lsp-enable-snippet nil) ; the quality is really poor for C
   (setq-local compile-command
               (cond ((file-exists-p "Makefile")
                      "make -k")
@@ -36,11 +19,10 @@
                         buffer-file-name
                         " -o out && ./out")))))
 
-(define-key c-mode-map (kbd "C-c C-h") #'helm-dash-at-point)
-(define-key c-mode-map (kbd "C-c C-c") #'recompile)
+;; (define-key c-mode-map (kbd "C-c C-h") #'helm-dash-at-point)
+;; (define-key c-mode-map (kbd "C-c C-c") #'recompile)
 
 (add-hook #'c-mode-hook #'c-config)
-
 ;; (add-hook #'c++-mode-hook   #'my-cmode-hook)
 
 ;; (defun my-cmode-hook ()

bf2e6a28f6318fcc33a67adaff7babf1c14ecde0
azimut <azimut.github@protonmail.com>
2024-11-20 08:36:17 -0300

    haskell: prettify-symbols to ligatures

--- lang/haskell.el
+++ lang/haskell.el
@@ -14,45 +14,6 @@
   :config
   (unbind-key "C-c C-c" haskell-interactive-mode-map))
 
-
-(defun haskell-config ()
-  ;; Pretty symbols that are as wider as thier characters more or less...
-  ;; https://gist.github.com/m-renaud/2c085d453b1263f1a6ed52d0c90688de
-  ;; https://github.com/pjones/emacsrc/blob/e6207555f1ab04fd056777e7e918bc4df47db593/modes/haskell-mode-conf.el#L269
-  (setq-local prettify-symbols-alist
-              '((">>=" .  (?\s (Br . Bl) ?\s (Br . Bl) ?\s
-                               (Bl . Bl) ?> (Bc . Bc) ?>
-                               (Bc . Bl) ?= (Br . Br) ?=))
-                ("=<<" .  (?\s (Br . Bl) ?\s (Br . Bl) ?\s
-                               (Bl . Bl) ?= (Bc . Br) ?= (Bc . Bc) ?<
-                               (Bl . Bl) ?= (Br . Br) ?<))
-                ("=>"  .  (?\s (Br . Bl) ?\s
-                               (Bl . Bl) ?= (Bc . Br) ?= (Bc . Bc) ?=
-                               (Br . Br) ?>))
-                ("->"  .  (?\s (Br . Bl) ?\s
-                               (Bl . Bl) ?- (Bc . Br) ?- (Bc . Bc) ?-
-                               (Bc . Bl) ?- (Br . Br) ?>))
-                ("<-"  .  (?\s (Br . Bl) ?\s
-                               (Bl . Bl) ?< (Bc . Br) ?- (Bc . Bc) ?-
-                               (Bc . Bl) ?- (Br . Br) ?-))
-                ("-<"  .  (?\s (Br . Bl) ?\s
-                               (Bl . Bl) ?- (Bc . Bc) ?- (Br . Br) ?<))
-                (">-"  .  (?\s (Br . Bl) ?\s
-                               (Bl . Bl) ?> (Bc . Bc) ?- (Br . Br) ?-))
-                ("++"  .  (?\s (Br . Bl) ?\s
-                               (Bl . Bl) ?+ (Bc . Br) ?+ (Bc . Bc) ?-
-                               (Bc . Bl) ?+ (Br . Br) ?+))
-                ("=="  .  (?\s (Br . Bl) ?\s
-                               (Bl . Bl) ?= (Bc . Br) ?= (Bc . Bc) ?=
-                               (Bc . Bl) ?= (Br . Br) ?=))
-                ("/="  .  (?\s (Br . Bl) ?\s
-                               (Bl . Bl) ?= (Bc . Br) ?= (Bc . Bc) ?/
-                               (Bc . Bl) ?= (Br . Br) ?=))
-                ("<>"  .  (?\s (Br . Bl) ?\s
-                               (Bl . Bl) ?< (Bc . Br) ?<
-                               (Bc . Bl) ?> (Br . Br) ?>))))
-  (setq-local company-auto-complete-chars nil))
-
 (use-package haskell-mode
   :bind (:map
          haskell-mode-map
@@ -60,9 +21,12 @@
          ("C-c C-d" . lsp-describe-thing-at-point)
          ("C-c C-k" . haskell-process-load-file))
   :hook (haskell-mode . smartparens-strict-mode)
-  :hook (haskell-mode . prettify-symbols-mode)
   :hook (haskell-mode . haskell-config)
-  :hook (haskell-mode . lsp))
+  :hook (haskell-mode . lsp)
+  :config
+  (ligature-set-ligatures
+   'haskell-mode
+   '(">>=" "=<<" "=>" "->" "<-" "-<" ">-" "++" "==" "/=" "<>" "||")))
 
 (add-hook 'haskell-literate-mode-hook #'lsp)
 

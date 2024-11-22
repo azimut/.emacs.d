52931ddaab519162a7cf321b4454686063db9d24
azimut <azimut.github@protonmail.com>
2024-11-21 21:13:15 -0300

    awk to eglot

--- lang/awk.el
+++ lang/awk.el
@@ -1,17 +1,17 @@
 ;; https://github.com/Beaglefoot/awk-language-server
 ;; $ npm install -g awk-language-server
-;; M-x treesi-install-language-grammar awk
+;; M-x treesit-install-language-grammar awk
 
 (use-package awk-yasnippets)
 (require 'awk-yasnippets)
+(eval-after-load 'yasnippet '(awk-yasnippets-initialize))
 
 (use-package awk-ts-mode
   :mode (("\\.awk\\'" . awk-ts-mode))
   :bind (:map
          awk-ts-mode-map
          ("C-c C-c" . recompile))
-  :hook (awk-ts-mode . lsp-mode)
-  ;; :hook (awk-mode . awk-yasnippets-initialize)
+  :hook (awk-ts-mode . eglot-ensure)
   :hook (awk-ts-mode . aggressive-indent-mode)
   :hook (awk-ts-mode . smartparens-strict-mode)
   :hook (awk-ts-mode . awk-config)
@@ -19,6 +19,11 @@
   (ligature-set-ligatures
    'awk-ts-mode
    '("<=" ">=" "==" "!=" "++" "&&" "||"))
+  :init
+  ;; NOTE: because is not added by default, but took this from lsp server github
+  (with-eval-after-load 'eglot
+    (add-to-list 'eglot-server-programs
+                 '(awk-ts-mode . ("awk-language-server"))))
   (defun awk-config ()
     (setq-local
      compile-command

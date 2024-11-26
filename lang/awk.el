123251ae3f20acb17fe5d9b6c6cf94e31343de54
azimut <azimut.github@protonmail.com>
2024-11-25 21:06:50 -0300

    awk: rollback to non treesit mode
    due C-a C-e movement was trash
    combobulate will be "ideal"
    or setting it up to work with the awk C-a C-e stuff i guess

--- lang/awk.el
+++ lang/awk.el
@@ -6,24 +6,24 @@
 (require 'awk-yasnippets)
 (eval-after-load 'yasnippet '(awk-yasnippets-initialize))
 
-(use-package awk-ts-mode
-  :mode (("\\.awk\\'" . awk-ts-mode))
+(use-package awk-mode
+  :ensure nil
   :bind (:map
-         awk-ts-mode-map
+         awk-mode-map
          ("C-c C-c" . recompile))
-  :hook (awk-ts-mode . eglot-ensure)
-  :hook (awk-ts-mode . aggressive-indent-mode)
-  :hook (awk-ts-mode . smartparens-strict-mode)
-  :hook (awk-ts-mode . awk-config)
+  :hook (awk-mode . eglot-ensure)
+  :hook (awk-mode . aggressive-indent-mode)
+  :hook (awk-mode . smartparens-strict-mode)
+  :hook (awk-mode . awk-config)
   :config
   (ligature-set-ligatures
-   'awk-ts-mode
+   'awk-mode
    '("<=" ">=" "==" "!=" "++" "&&" "||"))
   :init
   ;; NOTE: because is not added by default, but took this from lsp server github
   (with-eval-after-load 'eglot
     (add-to-list 'eglot-server-programs
-                 '(awk-ts-mode . ("awk-language-server"))))
+                 '(awk-mode . ("awk-language-server"))))
   (defun awk-config ()
     (setq-local eglot-stay-out-of '(eldoc))
     (setq-local

30d50070732a301746aca7665b03f12544e4cb45
azimut <azimut.github@protonmail.com>
2024-11-27 15:48:27 -0300

    clojure: use corfu

--- lang/clojure.el
+++ lang/clojure.el
@@ -16,6 +16,12 @@
 
 (use-package cider
   :hook (cider-repl-mode . electric-pair-local-mode)
+  :hook (cider-repl-mode . corfu-disable-margin)
+  :hook (cider-repl-mode . corfu-mode)
+  :hook (cider-mode      . corfu-disable-margin)
+  :hook (cider-mode      . corfu-mode)
+  :config
+  (setq cider-infer-remote-nrepl-ports t)
   :bind
   (:map
    cider-repl-mode-map
@@ -28,6 +34,8 @@
    ("C-c C-d C-h" . cider-clojuredocs)
    ("C-c ~"       . cider-switch-and-set))
   :init
+  (defun corfu-disable-margin ()
+    (setq-local corfu-margin-formatters nil))
   (defun cider-switch-and-set ()
     (interactive)
     (cider-switch-to-repl-buffer t)))

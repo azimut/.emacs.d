35f58c94ec56db0811442f4ae68d47d71c465223
azimut <azimut.github@protonmail.com>
2024-12-13 00:37:05 -0300

    python: add corfu to python shell

--- lang/python.el
+++ lang/python.el
@@ -5,6 +5,7 @@
   :hook (python-mode . indent-bars-mode)
   :hook (python-mode . smartparens-strict-mode)
   :hook (python-mode . python-config)
+  :hook (inferior-python-mode . corfu-mode)
   :init
   (defun python-config ()
     ;;  (add-hook 'before-save-hook #'lsp-format-buffer t t)

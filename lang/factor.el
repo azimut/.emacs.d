680d4e6edb6699d2ad5c05590a0a596056b3eaad
azimut <azimut.github@protonmail.com>
2025-12-04 01:07:17 -0300

    factor: init

---
+++ lang/factor.el
@@ -0,0 +1,5 @@
+(use-package fuel
+  :hook (factor-mode . display-line-numbers-mode)
+  :hook (factor-mode . aggressive-indent-mode)
+  :custom
+  (fuel-factor-root-dir "/home/sendai/projects/thirdparty/factor/"))

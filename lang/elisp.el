e82c46513c65aa0706a30a77b8747a1663fbc48a
azimut <azimut.github@protonmail.com>
2026-08-11 18:30:25 -0300

    elisp reformat, disable *scratch* elisp

--- lang/elisp.el
+++ lang/elisp.el
@@ -1,22 +1,19 @@
-;; Emacs
-(use-package elisp-mode
+(use-package emacs-lisp-mode
   :ensure nil
+  :hook (emacs-lisp-mode . paredit-mode)
+  :hook (emacs-lisp-mode . rainbow-delimiters-mode)
+  :hook (emacs-lisp-mode . aggressive-indent-mode)
+  :bind (:map emacs-lisp-mode-map
+              ("C-c C-c" . #'compile-defun)
+              ("C-c C-d" . #'emacs-lisp-describe-at-point))
   :init
+  (defun emacs-lisp-describe-at-point ()
+    (interactive)
+    (describe-symbol
+     (symbol-at-point)))
   ;; Default *scratch* buffer to lexical binding.
-  (add-hook 'lisp-interaction-mode-hook
-            (lambda ()
-              (when (equal (buffer-name) "*scratch*")
-                (setq-local lexical-binding t))))
-  (add-hook 'emacs-lisp-mode-hook
-            (lambda ()
-              (paredit-mode +1)
-              (rainbow-delimiters-mode +1)
-              (aggressive-indent-mode +1)
-              (eldoc-mode +1)
-              ;; SLIME like keybinding instead of "C-h f"
-              (define-key emacs-lisp-mode-map (kbd "C-c C-d")
-                (lambda ()
-                  (interactive)
-                  (describe-symbol
-                   (symbol-at-point))))
-              (define-key emacs-lisp-mode-map (kbd "C-c C-c") #'compile-defun))))
+  ;; (add-hook 'lisp-interaction-mode-hook
+  ;;           (lambda ()
+  ;;             (when (equal (buffer-name) "*scratch*")
+  ;;               (setq-local lexical-binding t))))
+  )

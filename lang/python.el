36efd513ed69e920366458a90083317714247ee1
azimut <azimut.github@protonmail.com>
2026-09-13 03:55:12 -0300

    python: display-line-number, prettify-symbols disable

--- lang/python.el
+++ lang/python.el
@@ -5,6 +5,7 @@
   ;;:hook (python-mode . eglot-ensure)
   :hook (python-mode . corfu-mode)
   :hook (python-mode . indent-bars-mode)
+  :hook (python-mode . display-line-numbers-mode)
   :hook (python-mode . smartparens-strict-mode)
   :hook (python-mode . python-config)
   :hook (inferior-python-mode . corfu-mode)
@@ -13,6 +14,7 @@
          ("C-c C-k" . recompile))
   :init
   (defun python-config ()
+    (prettify-symbols-mode -1)
     (setq-local sp-hybrid-kill-excessive-whitespace nil)))
 
 ;; (add-hook 'python-mode-hook (lambda () (elpy-mode)))
